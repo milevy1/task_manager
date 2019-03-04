f5f8e4718495e805f4533eb52fe222c46b7ac5ca
Matt Levy <milevy1@gmail.com>
2019-03-04 09:58:49 -0700

    Added delete

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -33,9 +33,14 @@ class TaskManagerApp < Sinatra::Base
   end
 
   set :method_override, true
-  
+
   put '/tasks/:id' do |id|
     Task.update(id.to_i, params[:task])
     redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
+    redirect '/tasks'
   end
 end
