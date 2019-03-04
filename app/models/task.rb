73f23654dd0ff7411c67eaae41c7461d2636025c
Matt Levy <milevy1@gmail.com>
2019-03-04 09:36:54 -0700

    Added links to individual tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,10 @@
 require 'sqlite3'
 
 class Task
-  attr_reader :title, :description
+  attr_reader :title, :description, :id
 
   def initialize(task_params)
+    @id = task_params["id"] if task_params["id"]
     @description = task_params["description"]
     @title       = task_params["title"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
